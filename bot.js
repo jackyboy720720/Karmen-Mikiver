40b5491a28a0331e5904c0d866cce64fe2bd8d9d
jackyboy720720 <42346338+jackyboy720720@users.noreply.github.com>
2018-08-13 22:17:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,34 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Karmen') {
+    if (message.content === 'Karmen1') {
     	message.reply('Paul, nahhui sa nii sitt ja debiilne urod oled!');
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Karmen2') {
+    	message.reply('Paul, miks su munn niiiiii väike on!?');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Karmen3') {
+    	message.reply('Paul, ma armastan sind!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Karmen4') {
+    	message.reply('Paul, ma vihkan sind!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Karmen5') {
+    	message.reply('Paul, mine perse!');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
